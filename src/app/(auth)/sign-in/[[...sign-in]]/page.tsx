7a32209d466c76c8d9d86674a62bec408e0d71ad
Barry Napier <barry.a.napier@gmail.com>
2023-11-15 16:02:37 +0000

    fix redirects

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,13 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="grid place-content-center h-full">
       <SignIn />
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,13 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="grid place-content-center h-full">
       <SignUp />

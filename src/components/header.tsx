1f9d3a8557f401a54537ba4a3fa5569380d45318
Barry Napier <barry.a.napier@gmail.com>
2024-03-21 15:25:03 +0000

    Update bookmark icon size in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,8 +9,8 @@ export function Header() {
           href="\"
           className="flex items-center gap-1 text-foreground hover:text-foreground"
         >
-          <BookmarkIcon className="h-7 w-7" />
-          <div className="flex text-xl font-semibold tracking-tight">
+          <BookmarkIcon className="h-4 w-4" />
+          <div className="flex font-semibold tracking-tight">
             Bookmarking.io
           </div>
         </Link>
